9bcf6c8107e8a30fd82da5e295d383d3bd9671fc
crazzyboy420 <raselbabu34.bd@gmail.com>
2022-12-28 18:35:17 +0600

    laravel-courses-frontend

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from './components/Header.vue';
+import Footer from  './components/Footer.vue';
+import HomePage from  './views/Home.vue';
+import Courses from  './views/Courses.vue';
+import SinglePage from './views/Single.vue';
+</script>
+
+<template>
+  <Header />
+  <router-view></router-view>
+  <Footer />
+</template>
+
